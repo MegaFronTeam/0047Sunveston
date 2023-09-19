9f72917942f7ad4e58281bfe08ccd1a38108b6de
Janis <janis.paberzs18@gmail.com>
2023-09-19 20:30:45 +0300

    Initial commit

---
+++ readme.md
@@ -0,0 +1,2 @@
+# 0047Sunveston
+ 
---
+++ sourse/js/common.js
@@ -0,0 +1,88 @@
+"use strict";
+
+const $ = jQuery;
+
+
+function eventHandler() {
+
+	JSCCommon.init()
+
+
+	function whenResize() {
+		JSCCommon.setFixedNav();
+	}
+
+	window.addEventListener('scroll', () => {
+		JSCCommon.setFixedNav();
+
+	}, { passive: true })
+	window.addEventListener('resize', () => {
+		whenResize();
+	}, { passive: true });
+
+	whenResize();
+
+
+	let defaultSl = {
+		spaceBetween: 0,
+		lazy: {
+			loadPrevNext: true,
+		},
+		watchOverflow: true,
+		loop: true,
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+		pagination: {
+			el: ' .swiper-pagination',
+			type: 'bullets',
+			clickable: true,
+			// renderBullet: function (index, className) {
+			// 	return '<span class="' + className + '">' + (index + 1) + '</span>';
+			// }
+		},
+	}
+
+	new Swiper('.breadcrumb-slider--js', {
+		slidesPerView: 'auto',
+		freeMode: true,
+		watchOverflow: true
+	});
+
+	const swiper4 = new Swiper('.sBanners__slider--js', { // если не используешь методы swiper  - можно обращаться без нее к Swiper
+		// slidesPerView: 5,
+		...defaultSl,
+		slidesPerView: 'auto',
+		freeMode: true,
+		loopFillGroupWithBlank: true,
+		touchRatio: 0.2,
+		slideToClickedSlide: true,
+		freeModeMomentum: true,
+
+	});
+
+	window.addEventListener('scroll', () => {
+		if(window.scrollY > 220) {
+			$('.scroll-to-top').addClass('show');
+		} else {
+			$('.scroll-to-top').removeClass('show');
+		}
+	}, { passive: true })
+	$('.scroll-to-top').on('click', function() {
+		window.scrollTo(0, 0);
+	});
+};
+if (document.readyState !== 'loading') {
+	eventHandler();
+} else {
+	document.addEventListener('DOMContentLoaded', eventHandler);
+}
+
+// window.onload = function () {
+// 	document.body.classList.add('loaded_hiding');
+// 	window.setTimeout(function () {
+// 		document.body.classList.add('loaded');
+// 		document.body.classList.remove('loaded_hiding');
+// 	}, 500);
+// }
