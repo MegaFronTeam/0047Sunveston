92a5c161e52f5a82fa6b962528ba4d1ab9fb353e
Janis <janis.paberzs18@gmail.com>
2023-10-04 21:27:37 +0300

    edit js

--- sourse/js/JSCCommon.js
+++ sourse/js/JSCCommon.js
@@ -1,4 +1,66 @@
 'use strict';
+
+function rangeSlider(slider, inputFrom, inputTo) {
+	var $range = $(slider);
+	var $inputFrom = $(inputFrom);
+	var $inputTo = $(inputTo);
+	var instance;
+	var min = 0;
+	var max = 20000;
+	var from = 0;
+	var to = 0;
+
+	$range.ionRangeSlider({
+			skin: "round",
+			type: "double",
+			min: min,
+			max: max,
+			from: 890,
+			to: 18090,
+			onStart: updateInputs,
+			onChange: updateInputs,
+			onFinish: updateInputs
+	});
+	instance = $range.data("ionRangeSlider");
+	
+	function updateInputs (data) {
+			from = data.from;
+			to = data.to;
+	
+			$inputFrom.prop("value", from + " ₽");
+			$inputTo.prop("value", to + " ₽");
+	}
+	
+	$inputFrom.on("change", function () {
+			var val = $(this).prop("value");
+			console.log(val);
+			// validate
+			if (val < min) {
+				val = min;
+			} else if (val > to) {
+				val = to;
+			}
+			instance.update({
+				from: val
+			});
+			$(this).prop("value", val);
+		});
+	
+	$inputTo.on("change", function () {
+		var val = $(this).prop("value");
+		// validate
+		if (val < from) {
+				val = from;
+		} else if (val > max) {
+				val = max;
+		}
+		instance.update({
+				to: val
+		});
+		$(this).prop("value", val);
+	});
+};
+
 class JSCCommon {
 	static modalCall() {
 		const link = '[data-fancybox="modal"], .link-modal-js';
@@ -12,123 +74,8 @@ class JSCCommon {
 			dragToClose: false,
 			on: {
 				reveal: () => {
-					var $range = $(".js-range-slider");
-					var $inputFrom = $(".js-input-from");
-					var $inputTo = $(".js-input-to");
-					var instance;
-					var min = 0;
-					var max = 20000;
-					var from = 0;
-					var to = 0;
-				
-					$range.ionRangeSlider({
-							skin: "round",
-							type: "double",
-							min: min,
-							max: max,
-							from: 890,
-							to: 18090,
-							onStart: updateInputs,
-							onChange: updateInputs,
-							onFinish: updateInputs
-					});
-					instance = $range.data("ionRangeSlider");
-					
-					function updateInputs (data) {
-							from = data.from;
-							to = data.to;
-					
-							$inputFrom.prop("value", from + " ₽");
-							$inputTo.prop("value", to + " ₽");
-					}
-					
-					$inputFrom.on("change", function () {
-							var val = $(this).prop("value");
-							console.log(val);
-							// validate
-							if (val < min) {
-								val = min;
-							} else if (val > to) {
-								val = to;
-							}
-							instance.update({
-								from: val
-							});
-							$(this).prop("value", val);
-						});
-					
-					$inputTo.on("change", function () {
-						var val = $(this).prop("value");
-						// validate
-						if (val < from) {
-								val = from;
-						} else if (val > max) {
-								val = max;
-						}
-						instance.update({
-								to: val
-						});
-						$(this).prop("value", val);
-					});
-
-					var $range2 = $(".js-range-slider2");
-					var $inputFrom2 = $(".js-input-from2");
-					var $inputTo2 = $(".js-input-to2");
-					var instance;
-					var min = 0;
-					var max = 20000;
-					var from = 0;
-					var to = 0;
-				
-					$range2.ionRangeSlider({
-							skin: "round",
-							type: "double",
-							min: min,
-							max: max,
-							from: 890,
-							to: 18090,
-							onStart: updateInputs2,
-							onChange: updateInputs2,
-							onFinish: updateInputs2
-					});
-					instance = $range2.data("ionRangeSlider");
-					
-					function updateInputs2 (data) {
-							from = data.from;
-							to = data.to;
-					
-							$inputFrom2.prop("value", from + " м²");
-							$inputTo2.prop("value", to + " м²");
-					}
-					
-					$inputFrom2.on("change", function () {
-							var val = $(this).prop("value");
-							console.log(val);
-							// validate
-							if (val < min) {
-								val = min;
-							} else if (val > to) {
-								val = to;
-							}
-							instance.update({
-								from: val
-							});
-							$(this).prop("value", val);
-						});
-					
-					$inputTo2.on("change", function () {
-						var val = $(this).prop("value");
-						// validate
-						if (val < from) {
-								val = from;
-						} else if (val > max) {
-								val = max;
-						}
-						instance.update({
-								to: val
-						});
-						$(this).prop("value", val);
-					});
+					rangeSlider(".js-range-slider", ".js-input-from", ".js-input-to");
+					rangeSlider(".js-range-slider2", ".js-input-from2", ".js-input-to2");
 
 					// mask for input
 					let InputTel = [].slice.call(document.querySelectorAll('input[type="tel"]'));
