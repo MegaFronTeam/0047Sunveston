e97209fcb7619bb547fc374e461dbcfaa80379cc
Eleonora Ufimtseva <lunar.politics@gmail.com>
2023-12-12 17:32:06 +0300

    submenu added

--- public/js/common.js
+++ public/js/common.js
@@ -50,6 +50,20 @@ function eventHandler() {
 		watchOverflow: true
 	});
 
+	document.addEventListener('click', function (event) {
+		let parent = $('.menu-item-has-children.shown');
+		let parentActive = event.target.closest('.menu-item-has-children.shown');
+		if (!parentActive) {
+			parent.removeClass('shown');
+		};
+		let toggle = event.target.closest('.menu-mobile--js.active .menu-item-has-children > a');
+		if (toggle) {
+			event.preventDefault();
+			let parent = toggle.closest('.menu-item-has-children');
+			parent.classList.toggle('shown');
+		};
+	});
+
 	let freeSliders = document.querySelectorAll('.free-slider--js');
 	if (freeSliders) {
 		freeSliders.forEach(slider => {
